58ab599ee0876b629ccd788628c2ded0bb295f5c
LakhanGitHub <109935532+LakhanGitHub@users.noreply.github.com>
2025-02-18 15:10:32 +0530

    Add files via upload

--- teachers/models.py
+++ teachers/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class teacher(models.Model):
-    name = models.CharField(max_length=10)
-    email = models.EmailField()
-    phone_number = models.IntegerField()
-    bio = models.CharField(max_length=100)
--- teachers/views.py
+++ teachers/views.py
@@ -1,52 +1,77 @@
 from django.shortcuts import render
+from .forms import PredictionForm
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import TeachersForm
-from .models import teacher
 
+import pickle
+import os
+from django.conf import settings
+import pandas as pd
+import numpy as np
+import joblib
+
+# Load the model once when the server starts
+MODEL_PATH = os.path.join(settings.BASE_DIR, 'Computer_Data.pkl')
+#pipe_path = os.path.join(settings.BASE_DIR, 'Computer_Pipe.pkl')
+
+pipe_path = os.path.join(settings.BASE_DIR, 'Computer_Pipe.pkl')
+
+# Load the model
+
+#pipe = joblib.load(pipe_path)
+
+df = pd.read_pickle(MODEL_PATH)
+
+pipe = pd.read_pickle(pipe_path)
+
+#comp_part = df.Company.unique()
+#compay = tuple(df.Company.unique())
+
+
+#print(df.head(2))
 # Create your views here.
 
-def home(request):  
-    form = TeachersForm(request.POST)  
-    if request.method == 'POST':
+def predict(request):
+    form = PredictionForm(request.POST or None)
+    if request.method =='POST'and form.is_valid():
+        #if form.is_valid:
+            #process the selected catogery
+        compnamy = form.cleaned_data['Company']
+        type = form.cleaned_data['TypeName']
+        ram = form.cleaned_data['RAM']
+        weight = form.cleaned_data['Weight']
+        touchscreen = form.cleaned_data['TouchScreen']
+        if touchscreen =="Yes":
+            touchscreen = 1
+        else:
+            touchscreen = 0
+
+        ips = form.cleaned_data['IPSDisplay']
+        if ips =="Yes":
+            ips = 1
+        else:
+            ips = 0
+
+        screensize = form.cleaned_data['screensize'] 
+        if screensize==0:
+            screensize =0.1
+        else:
+            screensize
+        resolution = form.cleaned_data['ScreenResolustion']            
+        cpu = form.cleaned_data['CPU']
+        gpu = form.cleaned_data['GPU']
+        os = form.cleaned_data['OS']
+        hdd = form.cleaned_data['HDD']
+        ssd = form.cleaned_data['SSD']
+        res_x = resolution.split('x')[0]
+        res_y = resolution.split('x')[1]
+        ppi = (((int(res_x))**2 + (int(res_y))**2)**.5)/float(screensize)
+        query = np.array([compnamy,type,ram,weight,touchscreen,ips,ppi,cpu,gpu,os,hdd,ssd])
+
+        query = query.reshape(1,12)
         
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            phone_number = form.cleaned_data['phone_number']
-            bio = form.cleaned_data['bio']
-            teach = teacher.objects.create(name=name, email=email, phone_number=phone_number, bio=bio)
-            return HttpResponse('Well done!!')
+        prediction ="The predicted price of this configuration is: Rs " + str(np.round(np.exp(pipe.predict(query)[0]),0))
+        return render(request, 'priceprediction/home.html', {'form':form,'result':prediction})
+
     else:
-        form = TeachersForm()   
-    return render(request, 'teachers/home.html', {'form':form})
-
-
-def data(request):
-    alldata = teacher.objects.all()
-    return render(request, 'teachers/alldata.html', {'alldata':alldata})
-
-def update(request,id):
-    t = teacher.objects.get(id=id)
-    form = TeachersForm(request.POST)
-    if request.method == 'POST':
-        form = TeachersForm(request.POST)
-        if form.is_valid():
-            t.name = form.cleaned_data['name']
-            t.email = form.cleaned_data['email']
-            t.phone_number = form.cleaned_data['phone_number']
-            t.bio = form.cleaned_data['bio']
-            t.save()
-    else: 
-        form = TeachersForm(initial={'name':t.name, 'email':t.email, 'phone_number':t.phone_number, 'bio':t.bio})
-
-    return render(request, 'teachers/update.html',{'form':form})
-
-def delete(request,id):
-    if request.method=="POST":
-        t = teacher.objects.get(id=id)
-        t.delete()
-
-    return HttpResponseRedirect("/home/")
-
-def boottest(request):
-    return render(request, 'teachers/boottest.html')
+        form = PredictionForm()
+    return render(request, 'priceprediction/home.html', {'form':form})
